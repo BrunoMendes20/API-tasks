0176eaf4d2aa2cc38f39a2e6f71c2ff4bfba10f7
Bruno <brunoomendes20@gmail.com>
2026-01-27 18:21:09 -0300

    test(tasks): add store endpoint tests

---
+++ tests/Feature/Tasks/StoreTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+
+uses(RefreshDatabase::class);
+
+it('can create task', function () {
+    $user = User::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    $payload = [
+        'title' => 'Nova tarefa',
+        'description' => 'Descrição de teste',
+    ];
+
+    $response = $this->postJson('api/V1/tasks', $payload);
+
+
+    $response->assertStatus(201)->assertJsonFragment([
+        'title' => 'Nova tarefa',
+    ]);
+
+    $this->assertDatabaseHas('tasks', [
+        'title' => 'Nova tarefa',
+        'user_id' => $user->id,
+    ]);
+});
+
+
+it('cannot create task without authentication', function () {
+    $payload = [
+        'title' => 'Nova tarefa',
+    ];
+
+    $response = $this->postJson('/api/V1/tasks', $payload);
+
+    $response->assertStatus(401);
+});
+
+it('cannot create task without title', function () {
+    $userA = User::factory()->create();
+
+    Sanctum::actingAs($userA);
+
+    $payload = [
+        'description' => 'Sem título',
+    ];
+
+    $response = $this->postJson('api/V1/tasks', $payload);
+
+    $response->assertStatus(422)->assertJsonValidationErrors(['title']);
+});
+
+
+it('can create always  assigns task to authenticated user', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $payload = [
+        'title' => 'Minha tarefa',
+        'user_id' => 999,
+    ];
+
+    $reponse = $this->postJson('/api/V1/tasks', $payload);
+
+    $reponse->assertStatus(201);
+
+    $this->assertDatabaseHas('tasks', [
+        'title' => 'Minha tarefa',
+        'user_id' => $user->id,
+    ]);
+});
