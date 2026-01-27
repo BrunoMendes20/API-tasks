dfa92257ab9b5fd7b58e8fb00c8b6ed403ae710a
Bruno <brunoomendes20@gmail.com>
2026-01-27 18:22:04 -0300

    test(tasks): add destroy endpoint tests

---
+++ tests/Feature/Tasks/DestroyTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+
+uses(RefreshDatabase::class);
+
+it('can delete a task', function () {
+    $user = User::factory()->create();
+
+    $task = Task::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    Sanctum::actingAs($user);
+
+    $response = $this->deleteJson("api/V1/tasks/{$task->id}");
+
+    $response->assertStatus(204);
+
+    $this->assertSoftDeleted('tasks', [
+        'id' => $task->id,
+    ]);
+});
+
+it('cannot delete another users task', function () {
+    $userA = User::factory()->create();
+    $userB = User::factory()->create();
+
+    $task = Task::factory()->create([
+        'user_id' => $userB->id,
+    ]);
+
+    Sanctum::actingAs($userA);
+
+    $reponse = $this->deleteJson("api/V1/tasks/{$task->id}");
+
+    $reponse->assertStatus(403);
+
+    // Garante que a tarefa NÃO foi deletada
+    $this->assertDatabaseHas('tasks', [
+        'id' => $task->id,
+        'user_id' => $userB->id,
+    ]);
+});
+
+it('cannot delete task without authentication', function () {
+    $user = User::factory()->create();
+
+    $task = Task::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+
+    $response = $this->deleteJson("/api/V1/tasks/{$task->id}", [
+        'title' => 'Novo título',
+    ]);
+
+    $response->assertStatus(401);
+});
+
+
+it('returns 404 when delete non existing task', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $response = $this->deleteJson('api/V1/tasks/99999999');
+    $response->assertStatus(404);
+});
